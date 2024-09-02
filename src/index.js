dad618269c64e87c4ac73b4852ce05b9f2474112
Artem <spriteklg@gmail.com>
2024-09-02 04:36:57 +0400

    feat: add revrse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let stringNumb = n.toString();
+  let result = '';
+
+  if (stringNumb[0] === '-'){
+      stringNumb = stringNumb.slice(1);
+  }
+
+  for (let i = 0; i < stringNumb.length; i++) {
+
+      result = stringNumb[i] + result;
+  }
+
+  return Number(result);
 }
+
